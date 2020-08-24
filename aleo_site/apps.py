b19eaf295df175ee19b89b2aabf78249a9123a97
dakshthemaker <daksh510@gmail.com>
2020-08-24 11:26:17 +0530

    added files

---
+++ aleo_site/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AleoSiteConfig(AppConfig):
+    name = 'aleo_site'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\daksh\PycharmProjects\aleo_web_app\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
